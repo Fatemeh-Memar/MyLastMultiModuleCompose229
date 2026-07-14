70898623886ba7abe6471ff596549792ad4d1082
Memar <fm.memar@gmail.com>
2026-07-14 20:28:53 +0330

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyNewCompose"
+include(":app")
+include(":data")
+include(":domain")
+
+include(":core:common")
+include(":core:model")
+include(":core:ui")
+include(":core:designsystem")
+include(":feature:home")
