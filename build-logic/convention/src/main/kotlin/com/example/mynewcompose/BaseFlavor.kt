079fd8ce25c194ce7a9aa13edb2883e98957ce20
fm.memar <fm.memar@gmail.com>
2026-07-17 15:03:18 +0330

    BaseFlavor.kt

---
+++ build-logic/convention/src/main/kotlin/com/example/mynewcompose/BaseFlavor.kt
@@ -0,0 +1,47 @@
+@Suppress("EnumEntryName")
+enum class FlavorDimension {
+    environment
+}
+
+@Suppress("EnumEntryName")
+enum class BaseFlavor(
+    val dimension: FlavorDimension,
+    val applicationIdSuffix: String? = null
+) {
+
+    dev(
+        FlavorDimension.environment,
+        applicationIdSuffix = ".dev"
+    ),
+
+    prod(
+        FlavorDimension.environment,
+        applicationIdSuffix = ".pro"
+    )
+}
+
+fun configureFlavors(
+    commonExtension: CommonExtension,
+    flavorConfigurationBlock: ProductFlavor.(flavor: BaseFlavor) -> Unit = {},
+
+    ){
+    commonExtension.apply {
+        FlavorDimension.entries.forEach { flavorDimension ->
+            flavorDimensions += flavorDimension.name
+        }
+
+        productFlavors{
+            BaseFlavor.entries.forEach { baseFlavor ->
+                register(baseFlavor.name) {
+                    dimension = baseFlavor.dimension.name
+                    flavorConfigurationBlock(this, baseFlavor)
+                    if (commonExtension is ApplicationExtension && this is ApplicationProductFlavor) {
+                        if (baseFlavor.applicationIdSuffix != null) {
+                            applicationIdSuffix = baseFlavor.applicationIdSuffix
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
